e3b2d5572e45829e30e91e4b247041ae5b88e1c8
timothyjapira05-alt <timothy.japira05@gmail.com>
2026-09-24 20:57:35 +0700

    sudah ada bagian atas dan foto

--- myWeek2/app/src/main/java/com/timothy/myweek2/MainActivity.kt
+++ myWeek2/app/src/main/java/com/timothy/myweek2/MainActivity.kt
@@ -1,47 +1,68 @@
 package com.timothy.myweek2
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.timothy.myweek2.ui.theme.MyWeek2Theme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.layout.ContentScale
+@Composable
+fun soal1TugasView(
+    modifier: Modifier = Modifier.fillMaxSize()
+){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(Color.Blue)
+    ) {
+        Row(
+            modifier = Modifier
+                .weight(1f)
+                .padding(20.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("v", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+            Text("Liked Songs", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+            Text("...", fontWeight = FontWeight.Bold, fontSize = 24.sp)
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MyWeek2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        }
+        Row(
+            modifier = Modifier
+                .padding(20.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(R.drawable.screenshot_2026_09_24_202254),
+                "foto",
+                modifier = Modifier.size(400.dp),
+                
+            )
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    MyWeek2Theme {
-        Greeting("Android")
-    }
+fun soal1TugasPreview(){
+    soal1TugasView()
 }
