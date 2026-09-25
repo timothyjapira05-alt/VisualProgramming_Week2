b6c89c3bd60f4d74170d5b06275c8433eb87f213
timothyjapira05-alt <timothy.japira05@gmail.com>
2026-09-25 11:10:41 +0700

    yey ada background

--- myWeek2/app/src/main/java/com/timothy/myweek2/soal2/Soal2.kt
+++ myWeek2/app/src/main/java/com/timothy/myweek2/soal2/Soal2.kt
@@ -1,13 +1,33 @@
 package com.timothy.myweek2
 
+import android.graphics.drawable.Drawable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
 
 @Composable
-fun Soal2View(){
-    Text("Hallooooo")
-}
+fun Soal2View(
+
+){
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.download),
+            contentDescription = "Background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+    }
+    
+        }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
