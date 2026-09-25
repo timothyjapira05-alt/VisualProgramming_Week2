377da0beb38d59025d0b4394c39028b92cc211fb
timothyjapira05-alt <timothy.japira05@gmail.com>
2026-09-25 10:47:36 +0700

    buat project soal2, tambah background, dan font poppins untuk extra miles

---
+++ myWeek2/app/src/main/java/com/timothy/myweek2/soal2/Soal2.kt
@@ -0,0 +1,16 @@
+package com.timothy.myweek2
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Soal2View(){
+    Text("Hallooooo")
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Soal2Preview(){
+    Soal2View()
+}
