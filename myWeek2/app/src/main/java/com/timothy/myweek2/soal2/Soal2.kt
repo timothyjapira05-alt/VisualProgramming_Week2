85c13213edb2b7d9e13acf421184b1df4e4761a0
timothyjapira05-alt <timothy.japira05@gmail.com>
2026-09-25 12:05:41 +0700

    tambah tab atau box bagian bawah

--- myWeek2/app/src/main/java/com/timothy/myweek2/soal2/Soal2.kt
+++ myWeek2/app/src/main/java/com/timothy/myweek2/soal2/Soal2.kt
@@ -9,13 +9,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
 
 @Composable
 fun Soal2View(
 
-){
+) {
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -25,9 +34,39 @@ fun Soal2View(
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.Crop
         )
-    }
-    
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(
+                        RoundedCornerShape(
+                            topStart = 24.dp,
+                            topEnd = 24.dp
+                        )
+                    )
+                    .background(Color(0xFF383C5C))
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            start = 20.dp,
+                            end = 20.dp,
+                            top = 20.dp,
+                            bottom = 10.dp
+                        )
+                ){
+                    Text("halo")
+                }
+            }
         }
+
+    }
+}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
